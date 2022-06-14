c58ae70cdc28f9ef35c0b6d4b97b1a742e6b0ea2
ParthbBhut <parthbbhut1622@gmail.com>
2022-06-14 21:55:11 +0530

    Create App.js

---
+++ exp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ReactDOM from 'react-dom/client';
+import Home from './home';
+
+function App(props) {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
