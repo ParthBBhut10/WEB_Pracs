aa25d716c622fd61cc64434336e69093309d40a3
ParthbBhut <parthbbhut1622@gmail.com>
2022-06-15 12:31:42 +0530

    Create App.js

---
+++ props/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+
+function App(props) {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <h2>Have a Nice Day</h2>
+    </div>
+  );
+}
+
+export default App;
