581657c3f022d881914ae5221d591036d9164631
Parth.B.Bhut <73700934+ParthBBhut10@users.noreply.github.com>
2022-06-15 12:38:54 +0530

    Update App.js

--- exp/src/App.js
+++ exp/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import ReactDOM from 'react-dom/client';
 import Home from './home';
+import Count from './count';
 
 function App(props) {
   return (
     <div className="App">
-      <Home/>
+      <Count/>
     </div>
   );
 }
